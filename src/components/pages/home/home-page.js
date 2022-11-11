1274d45c7b68b40311b4d18deb7115ec7ea328c8
ava-art <koshkadav@gmail.com>
2022-11-11 20:31:12 +0300

    Refactoring 1

--- src/components/pages/home/home-page.js
+++ src/components/pages/home/home-page.js
@@ -1,48 +1,42 @@
 import React from "react";
 import { connect } from 'react-redux';
 import './home-page.css'
-import { bindActionCreators } from "redux";
-import * as actions from "../../../action/actions";
-import Sidebar from "../../sidebar/sidebar";
-import BannerHome from "../../banner-home/banner-home";
 import ListFeed from "./list-feed";
 import ArticlesListGlobal from "./list-articles";
+import Sidebar from "../../sidebar/sidebar";
+import BannerHome from "../../banner-home/banner-home";
 
-const HomePage = () =>{
-  
-
+const HomePage = () => {
 
   return (
     <div className="home-page">
 
-  <BannerHome />
+      <BannerHome />
 
-  <div className="container page">
-      <div className="row">
-
+      <div className="container page">
+        <div className="row">
+          
           <div className="col-md-9">
-              <div className="feed-toggle">
-                  <ul className="nav nav-pills outline-active">
-                      <ListFeed />
-                  </ul>
-              </div>
-        
-              <ArticlesListGlobal />
+            <div className="feed-toggle">
+              <ul className="nav nav-pills outline-active">
+                <ListFeed />
+              </ul>
+            </div>
 
+            <ArticlesListGlobal />
 
           </div>
 
           <Sidebar />
 
+        </div>
       </div>
-  </div>
-
-</div>
+    </div>
   )
 }
 
-const mapStateToProps = ({loginIn}) =>{
-  return{
+const mapStateToProps = ({ loginIn }) => {
+  return {
     loginIn
   }
 }
